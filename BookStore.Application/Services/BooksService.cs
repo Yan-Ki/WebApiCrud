7605e7fc61090727b370d14ca7ec3a92adf3ec2a
Yan-Ki <aleksei3833@gmail.com>
2025-10-10 00:14:26 +0400

    Add Services

---
+++ BookStore.Application/Services/BooksService.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.DataAccess.Repositories;
+
+namespace BookStore.Application.Services
+{
+    public class BooksService
+    {
+        
+        public BooksService(IBooksRepositories bookRepository)
+        {
+            
+        }
+    }
+}
